e53c67ae8710d2dfe7e29b2c37d84d7638c5dc37
Jaine <yoonsoo@JAINE-MAC.local>
2024-02-17 23:30:45 -0500

    Design / Front-End Final

--- src/scriptLocation.js
+++ src/scriptLocation.js
@@ -32,36 +32,36 @@ function showHeroes(jsonObj) {
   slide3Img.style.backgroundImage = 'url("img/place3.png")';
 }
   
-const myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+// const myHeaders = new Headers();
+// myHeaders.append("Content-Type", "application/json");
 
-const raw = JSON.stringify({
-  "location": {
-    "latitude": 43.6545629,
-    "longitude": -79.4064103,
-    "WEATHER_API_KEY": "d9442ae097e08ac8353888ae1852995b737e4851",
-    "MAPS_API_KEY": "AIzaSyDxS_u6YZ2SkkIHTRjl82U7ZHWT3JQog0A"
-  },
-  "pet": {
-    "name": "Dapper",
-    "img": "https://placedog.net/800/640?id=177",
-    "breed": "Scottish Terrier",
-    "age": 1.5,
-    "weight": 22,
-    "heartRate": 120,
-    "bodyTemperature": 40
-  }
-});
+// const raw = JSON.stringify({
+//   "location": {
+//     "latitude": 43.6545629,
+//     "longitude": -79.4064103,
+//     "WEATHER_API_KEY": "d9442ae097e08ac8353888ae1852995b737e4851",
+//     "MAPS_API_KEY": "AIzaSyDxS_u6YZ2SkkIHTRjl82U7ZHWT3JQog0A"
+//   },
+//   "pet": {
+//     "name": "Dapper",
+//     "img": "https://placedog.net/800/640?id=177",
+//     "breed": "Scottish Terrier",
+//     "age": 1.5,
+//     "weight": 22,
+//     "heartRate": 120,
+//     "bodyTemperature": 40
+//   }
+// });
 
-const requestOptions = {
-  method: "GET",
-  headers: myHeaders,
-  body: raw,
-  redirect: "follow"
-};
+// const requestOptions = {
+//   method: "GET",
+//   headers: myHeaders,
+//   body: raw,
+//   redirect: "follow"
+// };
 
-fetch("http://localhost:8787/health?token=TM2qQoHtA2B9us", requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error));
+// fetch("http://localhost:8787/health?token=TM2qQoHtA2B9us", requestOptions)
+//   .then((response) => response.text())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.error(error));
 
